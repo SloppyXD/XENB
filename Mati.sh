12fc69ef6a5cb39642c75aeadeab02e960c02294
SloppyXD <149076016+SloppyXD@users.noreply.github.com>
2023-11-29 22:21:35 +0100

    Update Mati.sh
    downgrade from x8 to x4

--- Mati.sh
+++ Mati.sh
@@ -40,10 +40,7 @@ sudo nohup ./xengpuminer -d0 > xengpuminer-0.log 2>&1 &
 sudo nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
 sudo nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
 sudo nohup ./xengpuminer -d3 > xengpuminer-3.log 2>&1 &
-sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
-sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
-sudo nohup ./xengpuminer -d6 > xengpuminer-6.log 2>&1 &
-sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
+
 
 
 
