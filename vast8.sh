b0119fa742ad5d55457dd930b57776eef0544228
SloppyXD <149076016+SloppyXD@users.noreply.github.com>
2024-04-23 23:23:39 +0200

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -47,10 +47,6 @@ sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
 sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
 sudo nohup ./xengpuminer -d6 > xengpuminer-6.log 2>&1 &
 sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
-sudo nohup ./xengpuminer -d8 > xengpuminer-8.log 2>&1 &
-sudo nohup ./xengpuminer -d9 > xengpuminer-9.log 2>&1 &
-sudo nohup ./xengpuminer -d10 > xengpuminer-10.log 2>&1 &
-sudo nohup ./xengpuminer -d11 > xengpuminer-11.log 2>&1 &
 
 
 echo "-------------------------"
